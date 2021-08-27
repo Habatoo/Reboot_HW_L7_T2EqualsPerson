8cadf28645872517810db8b9062f20600d04a010
habatoo <habatoo@gmail.com>
2021-08-27 22:53:42 +0700

    initial commit

---
+++ src/main/java/Laptenkov/Person.java
@@ -0,0 +1,12 @@
+package Laptenkov;
+
+/**
+ * Класс {@link Person} реализует интерфейс Comparable<Person>,
+ * определить метод toString
+ * Реализовать equals & hashCode
+ * Гарантируется, что значения name, city не null
+ * Условие равенства: все поля name, city, age должны совпадать.
+ * (name и city без учета регистра)
+ */
+public class Person {
+}
